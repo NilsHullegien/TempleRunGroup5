af15c74ec285efe8ebffe15a01c0a30094f89c03
Marcel Kuipers <mekuipers@tudelft.nl>
2015-09-06 15:45:53 +0200

    implemented full collisionChecker test

--- core/src/test/java/com/group5/CollisionCheckerTest.java
+++ core/src/test/java/com/group5/CollisionCheckerTest.java
@@ -2,8 +2,10 @@ package com.group5;
 
 import com.group5.core.controllers.CollisionChecker;
 import com.group5.core.world.FloorTile;
+import com.group5.core.world.Player;
 import com.group5.core.world.World;
 import com.group5.core.world.WorldObject;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,25 +25,59 @@ public class CollisionCheckerTest {
         this.checker = world.getCollider();
     }
 
+    /**
+     * Test whether the overlap method correctly recognises whether two objects overlap or not
+     */
     @Test
     public void overlapTest() {
         world.add(new FloorTile(0, 0));
 
+        //an object overlaps with itself
         assertTrue(checker.overlap(world.getObjects().get(0), world.getObjects().get(0)));
 
+        //two objects with the same coordinates overlap
         WorldObject w1 = new FloorTile(0, 0);
         WorldObject w2 = new FloorTile(0, 0);
         assertTrue(checker.overlap(w1, w2));
 
         w2.setY(64);
-
+        //the boundaries touch on the bottom side, but they do not overlap
         assertFalse(checker.overlap(w1, w2));
 
         w2.setX(1024);
         w2.setY(0);
 
+        // same thing for the right side
         assertFalse(checker.overlap(w1, w2));
 
     }
+    
+    /**
+     * Test whether the checkCollision method correctly recognises when an object collides with another object
+     */
+    @Test
+    public void checkCollisionTest(){
+    	world.add(new Player(0,0));
+    	//only one object, so no collision
+    	assertFalse(checker.checkCollision(world.getObjects().get(0)));
+ 
+    	
+    	//two objects, but they don't touch
+    	world.add(new FloorTile(0,64));
+       	assertFalse(checker.checkCollision(world.getObjects().get(0)));
+       	assertFalse(checker.checkCollision(world.getObjects().get(1)));
+        
+    	
+    	
+    	world.add(new FloorTile(0,0));
+    	//two objects that overlap, so there will be a collision
+    	assertTrue(checker.checkCollision(world.getObjects().get(0)));
+    	assertTrue(checker.checkCollision(world.getObjects().get(2)));
+    	 
+    	
+    	
+    	
+    }
+    
 
 }
