1cb0e1a5f0523164762d943fb56851d021c84d8d
Matthijs Wolting <matthijs@ohelloapp.com>
2015-09-15 11:58:40 +0200

    Implement log threshold loading from property file

--- core/src/main/java/com/group5/core/EndlessRunner.java
+++ core/src/main/java/com/group5/core/EndlessRunner.java
@@ -6,7 +6,11 @@ import com.group5.core.screens.MainMenu;
 import com.group5.core.util.Logger;
 import com.group5.core.util.TextureCache;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Startup class for the Game, from here all textures are loaded
@@ -54,8 +58,45 @@ public class EndlessRunner extends Game {
         return textureCache;
     }
 
+    /**
+     * Loads a configuration file and sets relevant configuration in the game.
+     */
+    public void loadConfiguration() {
+        Properties config = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("./game.properties");
+
+            config.load(input);
+
+            String threshold = config.getProperty("log_threshold", "DEBUG");
+            try {
+                Logger.Level t = Enum.valueOf(Logger.Level.class, threshold);
+                Logger.get().info("Logging", "Log level is now " + threshold);
+                Logger.get().setThreshold(t);
+            } catch (IllegalArgumentException ex) {
+                Logger.get().error("Invalid logging level set as threshold: " + threshold);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.get().warning("Game", "Config file could not be found");
+        } catch (IOException ex) {
+            Logger.get().error("Game", "Something went wrong while reading config");
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException ex) {
+                    Logger.get().error("Game", "Something went wrong while closing config file");
+                }
+            }
+        }
+
+    }
+
     @Override
     public void create() {
+        loadConfiguration();
         Logger.get().info("Game", "Creating");
         EndlessRunner.setGame(this);
         batch = new SpriteBatch();
