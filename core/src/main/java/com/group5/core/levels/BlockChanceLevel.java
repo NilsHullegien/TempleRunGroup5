8a90d4bf6c8553282b0559344e0b944681e02418
Levi <levilime@gmail.com>
2015-10-21 21:22:39 +0200

    added more levels <feature

--- core/src/main/java/com/group5/core/levels/BlockChanceLevel.java
+++ core/src/main/java/com/group5/core/levels/BlockChanceLevel.java
@@ -4,21 +4,22 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.group5.core.controllers.GameSlice;
 import com.group5.core.world.FloorTile;
+import com.group5.core.world.Obstacle;
 
 /**
  * This is just a temporary class to check if the
  * implementation works, we'll use decorators to
  * make the procedural levels much easier to create.
  */
-public class OnlyFloorLevel extends GameSlice {
+public class BlockChanceLevel extends GameSlice {
     /**
      * constructor.
      *
      * @param world physicsworld
      * @author Levi
      */
-    public OnlyFloorLevel(final World world) {
-        super(new Vector2(0, 0), new Vector2(2250, 2000));
+    public BlockChanceLevel(final World world) {
+        super(new Vector2(0, 0), new Vector2(1025, 2000));
         createElems(world);
     }
 
@@ -28,8 +29,8 @@ public class OnlyFloorLevel extends GameSlice {
      * @param before GameSlice
      * @param world  physicsworld
      */
-    public OnlyFloorLevel(final GameSlice before, final World world) {
-        super(before, new Vector2(0, 0), new Vector2(2250, 2000));
+    public BlockChanceLevel(final GameSlice before, final World world) {
+        super(before, new Vector2(0, 0), new Vector2(1025, 2000));
         createElems(world);
     }
 
@@ -40,10 +41,9 @@ public class OnlyFloorLevel extends GameSlice {
      */
     private void createElems(final World world) {
         getElems().add(new FloorTile(world, new Vector2(this.getStartPoint().x / 50, 0)));
-        getElems().add(new FloorTile(world, new Vector2((this.getStartPoint().x + 1225) / 50, 0)));
-        //float range = (this.getEndPoint().x - 200 - this.getStartPoint().x) / 50;
-        //if (Math.random() > 0.5) {
-        //    getElems().add(new Obstacle(world, new Vector2((float) ((this.getStartPoint().x) / 50 + range * Math.random()), 1.2f)));
-        //}
+        float range = (this.getEndPoint().x - 200 - this.getStartPoint().x) / 50;
+        if (Math.random() > 0.5) {
+           getElems().add(new Obstacle(world, new Vector2((float) ((this.getStartPoint().x) / 50 + range * Math.random()), 1.2f)));
+        }
     }
 }
