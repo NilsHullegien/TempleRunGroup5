480c0dc40eddb34ba85d23082f2aabd74be21c21
Nils <nhullegien@student.tudelft.nl>
2015-10-27 09:54:32 +0100

    Fixed spawn kills by shooting obstacle<feature

--- core/src/main/java/com/group5/core/levels/OnlyFloorLevel.java
+++ core/src/main/java/com/group5/core/levels/OnlyFloorLevel.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.group5.core.controllers.GameSlice;
 import com.group5.core.world.FloorTile;
-import com.group5.core.world.ShootingObstacle;
 
 /**
  * This is just a temporary class to check if the
@@ -41,10 +40,5 @@ public class OnlyFloorLevel extends GameSlice {
      */
     private void createElems(final World world) {
         getElems().add(new FloorTile(world, new Vector2(this.getStartPoint().x / 50, 0)));
-        float range = (this.getEndPoint().x - this.getStartPoint().x) / 50;
-        float xPosObstacle = (float) (this.getStartPoint().x / 50 + range * Math.random());
-        ShootingObstacle obstacle = new ShootingObstacle(world, new Vector2(xPosObstacle, 1.2f));
-        getElems().add(obstacle);
-        getElems().add(obstacle.getBullet());
     }
 }
